06212aaf0a27be9d24485458f6c1a2ad64cd4266
Aleksbgbg <kozichka01@abv.bg>
2019-06-28 10:36:07 +0100

    [DefaultImageHandler] Add image handler which takes no action.

---
+++ AutoPick/Services/GameInteraction/ImageProcessing/DefaultImageHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoPick.Services.GameInteraction.ImageProcessing
+{
+    using AutoPick.Models;
+
+    public class DefaultImageHandler : ImageHandlerBase
+    {
+        public DefaultImageHandler(IImage template, GameStatus gameStatus) : base(template, gameStatus)
+        {
+        }
+    }
+}
